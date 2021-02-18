28c97da04d78f1483ca9dc37bd4b7d43a3859978
abbykahlergi <70241972+abbykahler@users.noreply.github.com>
2021-02-17 20:45:26 -0800

    updated

--- wall/main/views.py
+++ wall/main/views.py
@@ -7,26 +7,34 @@ from django.contrib import messages
 def index(request):
     return render(request, 'index.html')
 
-def register(request):
-    errors = User.objects.validations(request.POST)
-    if errors:
-        for field, value in errors.items():
-            messages.error(request, value)
-        return redirect('/')
-    User.objects.register(request.POST)
-    new_user = User.objects.get(username=request.POST['username'])
-    request.session['user_id'] = new_user.id
-    return redirect('/dashboard')
-
-def login(request):
-    result = User.objects.authenticate(request.POST['username'], request.POST['password'])
-    if result == False:
-        messages.error(request,"Invalid Username/Password")
-    else:
-        user = User.objects.get(username = request.POST['username'])
-        request.session['user_id'] = user.id
-        return redirect('/dashboard')
+def login(request) :
+    if request.POST['type']=="register" :
+        errors = User.objects.basic_validator(request.POST)
+        if len(errors) > 0:
+            for key, value in errors.items() :
+                messages.error(request,value)
+            return redirect('/')
+        else :
+            pw_hash = bcrypt.hashpw(request.POST['password'].encode(), bcrypt.gensalt())
+            User.objects.create(first_name=request.POST['first_name'],
+                                last_name=request.POST['last_name'],
+                                email=request.POST['email'],
+                                password=pw_hash
+            )
+            user = User.objects.last()
+            request.session['userid'] = user.id
+            return redirect('/wall')
+    elif request.POST['type']=="login" :
+        user = User.objects.filter(email=request.POST['email'])
+        if user :
+            logged_user = user[0]
+            if bcrypt.checkpw(request.POST['password'].encode(), logged_user.password.encode()):
+                request.session['userid'] = logged_user.id 
+                return redirect('/wall')
+            else :
+                messages.error(request,"E-mail or Password is incorrect")
     return redirect('/')
+
 
 def dashboard(request):
     context = {
@@ -39,12 +47,12 @@ def logout(request):
     request.session.clear()
     return redirect('/')
 
-def post(request):
-    Messages.objects.create(
-        message = request.POST['message'],
-        author = User.objects.get(id=request.session['user_id'])
-    )
-    return redirect('/dashboard')
+# def post(request):
+#     Messages.objects.create(
+#         message = request.POST['message'],
+#         author = User.objects.get(id=request.session['user_id'])
+#     )
+#     return redirect('/dashboard')
 
 def post(request, post_id):
     context = {
@@ -52,14 +60,17 @@ def post(request, post_id):
         'post' : Messages.objects.get(id=post_id),
         'comments' : Comment.objects.order_by('id')
     }
-    return render(request,'show.html', context)
+    return render(request,'index.html', context)
 
-def comment(request, post_id):
+
+
+def comment(request) :
     user = User.objects.get(id=request.session['user_id'])
-    post = Messages.objects.get(id=post_id)
+    post = Messages.objects.get(id=request.POST['message_id'])
     Comment.objects.create(
-        comment = request.POST['comment'],
-        author = user,
-        message = post
-    )
+        comment= request.POST['comment'], 
+        user_id= user, 
+        message_id= messages)
     return redirect('/dashboard')
+
+
