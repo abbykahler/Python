3f221e63d0197f0ec521afae52381e802e2f1955
abbykahlergi <70241972+abbykahler@users.noreply.github.com>
2021-03-14 17:21:44 -0700

    updated

---
+++ workout2/main/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render, redirect
+import random
+from datetime import datetime 
+
+
+# from collection.forms import ContactForm
+
+
+
+def index(request):
+    return render(request,'index.html')
+
+def abs(request):
+    return render(request, 'abs.html')
+
+def back(request):
+    return render(request, 'back.html')
+
+def bi(request):
+    return render(request, 'bi.html')
+
+def chest(request):
+    return render(request, 'chest.html')
+
+def legs(request):
+    # form_class = ContactForm
+    return render(request, 'legs.html', {
+        # 'form': form_class,
+    })
+
+def tri(request):
+    return render(request, 'tri.html')
+
+def shoulder(request):
+    return render(request, 'shoulders.html')
+
