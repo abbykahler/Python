f434da04ec6e54d331c8583503af6c2c5459f6ec
abbykahlergi <70241972+abbykahler@users.noreply.github.com>
2021-04-05 19:11:40 -0700

    models

--- workout/main/models.py
+++ workout/main/models.py
@@ -1,14 +1,6 @@
 from django.db import models
 from django import forms
 
-# # our new form
-# class ContactForm(forms.Form):
-#     contact_name = forms.CharField(required=True)
-#     contact_email = forms.EmailField(required=True)
-#     content = forms.CharField(
-#         required=True,
-#         widget=forms.Textarea
-#     )
 
 
 
