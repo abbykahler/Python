cbe4b57e26a5df97ee44f2a1efd28e7ae1a90154
abbykahlergi <70241972+abbykahler@users.noreply.github.com>
2021-04-07 21:53:37 -0700

    updated models

--- workout2/main/models.py
+++ workout2/main/models.py
@@ -3,9 +3,11 @@ from django.db import models
 class Workout(models.Model):
     exercise = models.CharField(max_length=200)
     # muscle = models.CharField(max_length=200)
+    # image = models.ImageField(upload_to='cars').ImageField(upload_to='cars')
     sets = models.IntergerField()
     reps = models.IntergerField()
     lbs = models.IntergerField()
+    id = models.IntergerField()
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
