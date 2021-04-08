16d71b96074dc99e3d121c1d128ab2c8222bf4ef
abbykahlergi <70241972+abbykahler@users.noreply.github.com>
2021-04-07 18:04:51 -0700

    added models

---
+++ workout2/main/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Workout(models.Model):
+    exercise = models.CharField(max_length=200)
+    sets = models.IntergerField()
+    reps = models.IntergerField()
+    lbs = models.IntergerField()
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
