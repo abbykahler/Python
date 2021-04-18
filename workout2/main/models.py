131c2f663bba8d08617f476a66e47adc05ec3e84
abbykahlergi <70241972+abbykahler@users.noreply.github.com>
2021-04-18 10:09:04 -0700

    update

--- workout2/main/models.py
+++ workout2/main/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Workout(models.Model):
     exercise = models.CharField(max_length=200)
     muscle = models.CharField(max_length=200)
@@ -15,4 +16,3 @@ class Workout(models.Model):
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    
