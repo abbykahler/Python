86d8f0d38305a28942b9df22dd28e03883958f37
abbykahlergi <70241972+abbykahler@users.noreply.github.com>
2021-06-23 18:13:01 -0700

    session set up

---
+++ ak3/main/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from . import views
+# from collection import views
+
+
+urlpatterns = [
+    path('', views.index),
+    path('ninjaman', views.ninjaman),
+    path('work', views.work),
+    path('education', views.education),
+    path('contact', views.contact),
+    path('airplane', views.airplane),
+    path('trip', views.trip),
+
+    
+]
+    
