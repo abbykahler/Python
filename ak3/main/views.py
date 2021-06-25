22b3b3ad5042baf6e335c38c47de12f5375dd538
abbykahlergi <70241972+abbykahler@users.noreply.github.com>
2021-06-24 22:06:46 -0700

    clean up

--- ak3/main/views.py
+++ ak3/main/views.py
@@ -16,12 +16,6 @@ def ninjaman(request):
 def airplane(request):
     return render(request, 'airplane.html')
 
-def work(request):
-    return render(request, 'work.html')
-
-def education(request):
-    return render(request, 'education.html')
-
 def contact(request):
     # form_class = ContactForm
     return render(request, 'index.html', {
